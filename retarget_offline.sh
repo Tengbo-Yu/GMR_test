dfde9126bb28689fff31d6b8a5b2384219ea0651
Tengbo-Yu <ytb23@mails.tsinghua.edu.cn>
2026-04-23 14:37:51 +0800

    update json

--- retarget_offline.sh
+++ retarget_offline.sh
@@ -8,15 +8,26 @@
 #   --record_video \
 #   --video_path /mnt/c/Users/TsingSV/Work/GMR/test_Skeleton0_g1.mp4
 
+
+# python scripts/bvh_to_robot.py \
+#   --bvh_file /mnt/c/Users/TsingSV/GMR/aiming1_subject1.bvh \
+#   --format lafan1 \
+#   --robot unitree_g1 \
+#   --motion_fps 120 \
+#   --rate_limit \
+#   --save_path /mnt/c/Users/TsingSV/GMR/lafan1.pkl
+
+
 python scripts/bvh_to_robot.py \
-  --bvh_file /mnt/c/Users/TsingSV/GMR/lumo_live.bvh \
+  --bvh_file /mnt/c/Users/TsingSV/GMR/Skeleton1.bvh \
   --format mocap \
   --robot unitree_g1 \
   --motion_fps 120 \
   --rate_limit \
-  --save_path /mnt/c/Users/TsingSV/GMR/test_Skeleton0_g1.pkl \
-  --record_video \
-  --video_path /mnt/c/Users/TsingSV/GMR/test_Skeleton0_g1.mp4
+  --save_path /mnt/c/Users/TsingSV/GMR/skeleton1.pkl
+  #  \
+  # --record_video \
+  # --video_path /mnt/c/Users/TsingSV/GMR/test_Skeleton0_g1.mp4
 
 
 # python scripts/vis_robot_motion.py \
